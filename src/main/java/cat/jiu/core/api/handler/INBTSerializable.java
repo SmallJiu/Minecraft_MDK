adbd085cac577dabf9218bf5e5c60488e49287f7
SmallJiu <70803028+SmallJiu@users.noreply.github.com>
2025-07-21 23:24:25 +0800

    1.21.1-neo

--- src/main/java/cat/jiu/core/api/handler/INBTSerializable.java
+++
@@ -1,8 +0,0 @@
-package cat.jiu.core.api.handler;
-
-import net.minecraft.nbt.CompoundTag;
-
-public interface INBTSerializable {
-	CompoundTag write(CompoundTag nbt);
-	void read(CompoundTag nbt);
-}
